1642495332200d073ec6cc4e6008893710a4f7f4
Pramit Das <pramitdas.17@gmail.com>
2013-08-26 14:02:49 +0530

    Modified Student.java

--- HelloWorld/src/com/pramit/model/Student.java
+++ HelloWorld/src/com/pramit/model/Student.java
@@ -3,14 +3,21 @@ package com.pramit.model;
 public class Student {
 	private String name;
 	private String surname;
+	private String rollNo;
+	private Integer totalMarks;
 	
-	public Student(String name, String surname) {
+	public Student(String rollNo, String name, String surname) {
+		this.rollNo = rollNo;
 		this.name = name;
 		this.surname = surname;
 	}
 	
+	public Integer getMarks() {
+		return totalMarks;
+	}
+	
 	@Override
 	public String toString() {
-		return surname + ", " + name;
+		return surname + ", " + name + "(" + rollNo + ")";
 	}
 }

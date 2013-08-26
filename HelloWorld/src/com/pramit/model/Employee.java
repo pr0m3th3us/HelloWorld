5cbb2790472833c9820737ebebce523ee80d718c
Pramit Das <pramitdas.17@gmail.com>
2013-08-26 14:10:40 +0530

    New Emloyee.java

---
+++ HelloWorld/src/com/pramit/model/Employee.java
@@ -0,0 +1,16 @@
+package com.pramit.model;
+
+public class Employee {
+	private String name;
+	private String surname;
+	
+	public Employee(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+	
+	@Override
+	public String toString() {
+		return surname + ", " + name;
+	}
+}
